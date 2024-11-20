d3635bdb0ed526c49b06a6c8f695e91478db0901
woooyoung <axdsw121@gmail.com>
2024-11-20 21:31:51 +0900

    init

---
+++ src/main/java/com/example/jsExam/JsExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsExam/JsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

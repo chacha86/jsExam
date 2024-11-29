ad1189277de73fd1eae0ca5e3b89c28aee78dac6
chacha86 <taejincha0619@gmail.com>
2024-11-29 19:51:16 +0900

    json sirealization error

--- src/main/java/com/example/jsExam/Member.java
+++ src/main/java/com/example/jsExam/Member.java
@@ -3,20 +3,22 @@ package com.example.jsExam;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Getter
-@Setter
-@Entity
-public class Post {
+public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String body;
+    private String name;
 
-    @ManyToOne
-    private Member author;
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts = new ArrayList<>();
 }
--- src/main/java/com/example/jsExam/TestController.java
+++ src/main/java/com/example/jsExam/TestController.java
@@ -1,11 +1,44 @@
 package com.example.jsExam;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class TestController {
+
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+
+    @PostConstruct
+    public void init() {
+        Member m1 = Member.builder()
+                .name("홍길동")
+                .build();
+
+        memberRepository.save(m1);
+
+        Post post1 = Post.builder()
+                .title("제목1")
+                .body("내용1")
+                .author(m1)
+                .build();
+
+        Post post2 = Post.builder()
+                .title("제목2")
+                .body("내용2")
+                .author(m1)
+                .build();
+
+        postRepository.save(post1);
+        postRepository.save(post2);
+
+    }
 
     @GetMapping("/test")
     @ResponseBody
@@ -13,16 +46,13 @@ public class TestController {
         return "hello";
     }
 
+
+
     @GetMapping("/post")
     @ResponseBody
-    public Post post() {
+    public List<Post> post() {
 
-        Post post = Post.builder()
-                .title("제목1")
-                .body("내용1")
-                .author("홍길동")
-                .build();
-
-        return post; // 스프링부트 컨트롤러에서 객체를 리턴하면 JSON으로 변환된다.
+        List<Post> posts = postRepository.findAll();
+        return posts; // 스프링부트 컨트롤러에서 객체를 리턴하면 JSON으로 변환된다.
     }
 }
